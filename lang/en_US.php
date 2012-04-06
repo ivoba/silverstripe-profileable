ddce61384615b91c8f5d90620dff5179ec17902e
ivo <ivo.bathke@gmail.com>
2012-04-06 16:11:03 +0200

    us lang file

---
+++ lang/en_US.php
@@ -0,0 +1,17 @@
+<?php
+// -------------------------------------------------------------------
+// US translation for the Profileable Decorator
+// -------------------------------------------------------------------
+
+i18n::include_locale_file('Profileable', 'en_US');
+
+global $lang;
+
+$lang['de_DE']['Profileable']['NAME'] = 'name';
+$lang['de_DE']['Profileable']['PROFILE'] = 'profile';
+$lang['de_DE']['Profileable']['CITY'] = 'city';
+$lang['de_DE']['Profileable']['PHONE'] = 'phone';
+$lang['de_DE']['Profileable']['FAX'] = 'fax';
+$lang['de_DE']['Profileable']['EMAIL'] = 'e-mail';
+$lang['de_DE']['Profileable']['WWW'] = 'internet';
+$lang['de_DE']['Profileable']['PROFILEPICTURE'] = 'profile picture';
