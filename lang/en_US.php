18c3a4a971fb79e575fe0ba327f9c789894643f8
ivo <ivo.bathke@gmail.com>
2012-04-09 16:38:06 +0200

    fix

--- lang/en_US.php
+++ lang/en_US.php
@@ -5,11 +5,11 @@
 
 global $lang;
 
-$lang['de_DE']['Profileable']['NAME'] = 'name';
-$lang['de_DE']['Profileable']['PROFILE'] = 'profile';
-$lang['de_DE']['Profileable']['CITY'] = 'city';
-$lang['de_DE']['Profileable']['PHONE'] = 'phone';
-$lang['de_DE']['Profileable']['FAX'] = 'fax';
-$lang['de_DE']['Profileable']['EMAIL'] = 'e-mail';
-$lang['de_DE']['Profileable']['WWW'] = 'internet';
-$lang['de_DE']['Profileable']['PROFILEPICTURE'] = 'profile picture';
+$lang['en_US']['Profileable']['NAME'] = 'name';
+$lang['en_US']['Profileable']['PROFILE'] = 'profile';
+$lang['en_US']['Profileable']['CITY'] = 'city';
+$lang['en_US']['Profileable']['PHONE'] = 'phone';
+$lang['en_US']['Profileable']['FAX'] = 'fax';
+$lang['en_US']['Profileable']['EMAIL'] = 'e-mail';
+$lang['en_US']['Profileable']['WWW'] = 'internet';
+$lang['en_US']['Profileable']['PROFILEPICTURE'] = 'profile picture';
